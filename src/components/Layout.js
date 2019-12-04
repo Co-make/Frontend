ba576f82c3a57a9b546f42cd2c95244fbdcea5aa
Philip Johnson <phil7j@gmail.com>
2019-12-03 21:35:55 -0800

    re-build bottom nav, still need to link everythin up and divide layout component

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,15 @@
 import React, {useState} from "react";
 import styled from "styled-components";
-import Logo from "../images/logo.png";
+// import {ReactComponent as CoMakeLogo} from "../images/logo2.svg";
+import CoMakeLogo from "../images/CoMakeCircle.png";
 import { Link } from "react-router-dom";
 import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
 import IconButton from '@material-ui/core/IconButton';
 import MenuRoundedIcon from '@material-ui/icons/MenuRounded';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import HomeIcon from '@material-ui/icons/Home';
 import InfoIcon from '@material-ui/icons/Info';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
@@ -28,27 +33,13 @@ const Layout = props => {
   return (
     <div>
       <Nav>
-        <div className="logotext">
-          <img className="logo" src={Logo}></img>omake
-        </div>
-        <a href="https://flamboyant-mayer-055230.netlify.com/index.html">
+        <Logo src={CoMakeLogo}/>
+        {/* <a href="https://flamboyant-mayer-055230.netlify.com/index.html">
           Home
-        </a>
-        <a href="https://flamboyant-mayer-055230.netlify.com/aboutus.html">
-          About Us
-        </a>
-        {props.token ? (
-          <Link to="/login">
-            <Button color="facebook" fontSize="medium" onClick={logOut}>
-              Logout
-            </Button>
-          </Link>
-        ) : props.token === "" ? (
-          <></>
-        ) : (
-          <></>
-        )}
-      <IconButton onClick={()=> setMenu(!menu)}><MenuRoundedIcon fontSize="large" /></IconButton>
+        </a> */}
+
+
+      <IconButton color="default" onClick={()=> setMenu(!menu)}><MenuRoundedIcon fontSize="large" /></IconButton>
       <SideBar anchor="right" open={menu} onClose={()=> setMenu(false)}>
       <List component="nav" aria-label="main">
         <ListItem button>
@@ -69,7 +60,7 @@ const Layout = props => {
           <ListItemIcon>
             <ExitToAppIcon />
           </ListItemIcon>
-          <ListItemText primary="Logout" />
+          <ListItemText primary="Log Out" />
         </ListItem>
           </Link>
         ) : (
@@ -89,8 +80,8 @@ const Layout = props => {
 
       {props.children}
 
-      {/* <FooterNav className="bottom-nav">
-        <Button.Group widths="3" size="big">
+      <FooterNav className="bottom-nav">
+        {/* <Button.Group widths="3" size="big">
           <Link to="/">
           <Button
             icon="list alternate outline"
@@ -112,44 +103,67 @@ const Layout = props => {
             // onClick={() => props.history.push(`/profile/${props.localId}`)}
           />
           </Link>
-        </Button.Group>
-      </FooterNav> */}
+        </Button.Group> */}
+          <Fab aria-label="add">
+        <AddIcon fontSize="large"/>
+          </Fab>
+          <ButtonGroup size="large" variant="text">
+          <IconButton><HomeIcon fontSize="large"/></IconButton>
+          <IconButton ><AccountCircleIcon fontSize="large"/></IconButton>
+        </ButtonGroup>
+      </FooterNav>
     </div>
   );
 };
 
+const Logo = styled.img`
+width: 75px;
+
+`
 
 const SideBar = styled(Drawer)`
 .MuiDrawer-paper {
   width: 300px;
+  @media (max-width: 600px) {
+    width: 150px
+  }
 }
  .MuiDrawer-paperAnchorRight {
 background: #5477bb;
+  span {
+    color: yellow;
+  }
+  .MuiSvgIcon-root {
+    color: yellow;
+    font-size: 2rem;
+  }
 }
 `
 
 const Nav = styled.nav`
-  display: block;
+  /* display: block; */
   display: flex;
-  justify-content: space-evenly;
+  justify-content: space-between;
   align-items: center;
   background-color: #5477bb;
   font-family: "helvetica", sans serif;
   a {
     color: #ffff;
   }
-  height: 50px;
+  height: 80px;
   margin-bottom: 30px;
   font-size: 1.2rem;
   font-weight: bold;
   width: 100%;
+  padding: 25px;
   @media (max-width: 600px) {
-    background: red;
+    /* background: red; */
   }
 `;
 
 const FooterNav = styled.footer`
   display: flex;
+  width: 100%;
   border: none;
   justify-content: space-evenly;
   align-items: center;
@@ -159,6 +173,30 @@ const FooterNav = styled.footer`
   font-weight: bold;
   position: fixed;
   bottom: 0;
+  background-color: #5477bb;
+  &&&{
+    .MuiButtonGroup-root {
+      width: 50%;
+    }
+    .MuiIconButton-root {
+      /* background-color: yellow; */
+      width: 50%;
+      border-radius: 5%;
+    }
+  .MuiFab-root {
+position: absolute;
+bottom: 20px;
+align-self: center;
+width: 70px;
+height: 70px;
+border-radius: 100%;
+z-index: 5;
+color: white;
+background-color: #5477bb;
+/* border: 1px solid red; */
+}
+
+  }
 `;
 
 export default Layout;
